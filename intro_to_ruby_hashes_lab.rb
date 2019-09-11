7483216f323c5cd1208ad6b0c6d34d85ab2858cd
Emily Haynes <ehayne00@hotmail.com>
2019-09-11 21:23:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,9 @@ def monopoly_with_third_tier
      :railroads => {
      pieces: 4, 
      :rent_in_dollars => {
-       :level1 =>
-       :level2 =>
-       :level3 =>
+       :level1 => ,
+       :level2 => ,
+       :level3 => ,
        :level4 =>
      }
      }
