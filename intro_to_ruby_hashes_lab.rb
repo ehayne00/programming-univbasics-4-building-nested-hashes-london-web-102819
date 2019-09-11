3db0562976f956ee20df376b12061d1d55eb1d19
Emily Haynes <ehayne00@hotmail.com>
2019-09-11 21:38:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
      },
      :names => {
        :reading_railroad => {},
-       :railroad2 => {},
+       :pennsylvania_railroad => {},
        :railroad3 => {},
        :railroad4 => {}
      }
